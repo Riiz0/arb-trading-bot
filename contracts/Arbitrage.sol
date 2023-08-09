561d05c1f9826a2093eb62c42c31279cf270bd14
Riiz0 <shawnrizo@hotmail.com>
2023-08-09 15:41:11 -0400

    3.Contract Functions & Local Hardhat Blockchain Node

--- contracts/Arbitrage.sol
+++ contracts/Arbitrage.sol
@@ -1,8 +1,38 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Arbitrage {
+import "@balancer-labs/v2-interfaces/contracts/vault/IValut.sol";
+import "@balancer-labs/v2-interfaces/contracts/vault/IFlashLoanRecipient.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 
+contract Arbitrage is IFlashLoanRecipient{
+    IVault private constant vault =
+    IVault("");
+
+    IUniswapV2Router02 public immutable sRouter;
+    IUniswapV2Router02 public immutable uRouter;
+    address public owner;
+
+    constructor(address _sRouter, address _uRouter) {
+        sRouter = IUniswapV2Router(_sRouter); //Sushiswap
+        uRouter = IUniswapV2Router(_uRouter); //Uniswap
+        owner = msg.sender;
+    }
+
+    function executeTrade() external {
+    //Code for executing Trades
+    }
+
+    function receiveFlashLoan() external override {
+        //Code for receiving flashloan
+    }
+
+// -- INTERNAL FUNCTIONS -- //
+    function _swapOnUniswap() internal require {
+        //Code for swapping on uniswap
+    }
+
+    function _swapOnSushiswap() internal require {
+        //Code for swapping on sushiswap
+    }   
 }
-
-
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
+
+const privateKey = process.env.PRVIATE_KEY || ""
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      }
+    }
+  }
 };
